390dce8f87120a5ff27319ca9d995ff8a6ea292a
treegus <cowtree28@gmail.com>
2026-01-13 10:50:57 +0900

    feat: (#14) Add risk on device
    - risk 추가 및 redis에 맞춰 변경

--- BMS/src/main/java/team/dbe/bms/domain/history/domain/History.java
+++ BMS/src/main/java/team/dbe/bms/domain/history/domain/History.java
@@ -1,38 +1,21 @@
 package team.dbe.bms.domain.history.domain;
 
-import jakarta.persistence.*;
-import lombok.*;
-import org.jspecify.annotations.Nullable;
-import org.springframework.data.domain.Persistable;
-import team.dbe.bms.domain.device.domain.Device;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-import java.io.Serializable;
 import java.time.LocalDateTime;
 
-
-@Entity
-@IdClass(HistoryId.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Getter
-public class History implements Persistable<HistoryId> {
-    @Id
+public class History {
     private LocalDateTime time;
 
-    @Id
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "device_id")
-    private Device device;
+    private Long deviceId;
 
     private Double temperature;
 
-    @Override
-    public @Nullable HistoryId getId() {
-        return new HistoryId(this.time, this.device.getId());
-    }
-
-    @Override
-    public boolean isNew() {
-        return true;
-    }
+    private Double risk;
 }
